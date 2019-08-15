207a536c167ab22d97c5616944a0a65bba8f0223
soniashiss <soniashiss@gmail.com>
2019-08-15 11:47:51 +0800

    add get

--- requests/get.go
+++ requests/get.go
@@ -57,3 +57,35 @@ func SendGETWithHeader(uri string, param string, header map[string]string) ([]by
 		}
 	}
 }
+
+func SendGETWithHeaderCodeReturn(uri string, param string, header map[string]string) (int, []byte, error) {
+	newurl := uri + param
+
+	client := &http.Client{
+		Timeout: requestTimeout,
+	}
+
+	req, _ := http.NewRequest("GET", newurl, nil)
+
+	for k, v := range header {
+		req.Header.Add(k,v)
+	}
+
+	resp, err := client.Do(req)
+	code := int(404)
+	if resp != nil {
+		code = resp.StatusCode
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		return code, []byte{}, err
+	} else {
+		result, err := ioutil.ReadAll(resp.Body)
+		//fmt.Println(string(result))
+		if err != nil {
+			return code, []byte{}, err
+		} else {
+			return code, result, nil
+		}
+	}
+}
